8ea0bb5f9cf72c67315151164fcb59c0727f46f3
Alejo Herrera <alejoherrera1166@gmail.com>
2024-02-01 04:20:40 -0300

    Agregamos react-bootstrap y bootstrap.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  return <></>;
+  return <section>
+    <h1 className="text-center">Lista de Tareas</h1>
+  </section>;
 }
 
 export default App;
