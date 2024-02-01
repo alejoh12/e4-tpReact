9a2f824fb553f1de42ae72657f8acad508959ea4
Alejo Herrera <alejoherrera1166@gmail.com>
2024-02-01 08:31:26 -0300

    Agregué tarea en state

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,16 +1,36 @@
 import { Button, Form } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 // import Button from "react-bootstrap/Button";
 // import Form from "react-bootstrap/Form";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [lista, setLista] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // guardar en la lista - (spread)
+    setLista([...lista, tarea])
+    setTarea("");
+  }
+
   return (
     <section className="my-4">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="formTareas">
-          <Form.Control type="email" placeholder="Ej: Tarea 1" />
-          <Button variant="dark" className="ms-2" type="submit">Agregar</Button>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Tarea 1"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
+          <Button variant="dark" className="ms-2" type="submit">
+            Agregar
+          </Button>
         </Form.Group>
       </Form>
+      <ListaTareas></ListaTareas>
     </section>
   );
 };
